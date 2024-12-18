b8a3fb3d04413d67194960e120b6137828343f6e
MaverickDF <domenemaverick@gmail.com>
2024-12-18 22:44:07 +0100

    PI004

--- src/index.js
+++ src/index.js
@@ -8,3 +8,7 @@ app.listen(app.get("port"), leerPuerto());
 function leerPuerto() {
   console.log("Hola desde el puerto " + app.get("port"));
 }
+app.get("/rutaGet", function (req, res) {
+  console.log("Hola desde una ruta Get");
+  res.send("Hola desde una web llamada en el servidor");
+});
