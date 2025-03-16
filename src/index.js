aaefd970dd20589f5295d93655a7724c16681cf1
MaverickDF <domenemaverick@gmail.com>
2025-03-16 13:22:47 +0100

    PI007 v1.3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.set("port", process.env.PORT || 8080);
 
 app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
 
 app.listen(app.get("port"), leerPuerto());
 
